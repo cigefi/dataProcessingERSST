1697b72b2bf178ab3083f28e38a30236d4798b3c
serke <serke@Serke.local>
2016-03-09 14:55:45 -0600

    Performance improvement

--- dataProcessingERSST.m
+++ dataProcessingERSST.m
@@ -6,23 +6,42 @@
 % var2Read = Variable to be read (use 'ncdump' to check variable names)
 function [] = dataProcessingERSST(dirName,var2Read)
     if nargin < 1
-        error('dataProcessing: dirName is a required input')
+        error('dataProcessing: dirName is a required input');
+    else
+        dirName = strrep(dirName,'\','/'); % Clean dirName var
     end
     if nargin < 2
-        error('dataProcessing: var2Read is a required input')
+        error('dataProcessing: var2Read is a required input');
     end
     
-    dirData = dir(dirName);  % Get the data for the current directory
-    path = java.lang.String(dirName);
+    dirData = dir(char(dirName(1)));  % Get the data for the current directory
+    path = java.lang.String(dirName(1));
     if(path.charAt(path.length-1) ~= '/')
         path = path.concat('/');
+    end
+    if(length(dirName)>1)
+        savePath = java.lang.String(dirName(2));
+        if(length(dirName)>2)
+            logPath = java.lang.String(dirName(3));
+        else
+            logPath = java.lang.String(dirName(2));
+        end
+	else
+		savePath = java.lang.String(dirName(1));
+		logPath = java.lang.String(dirName(1));
+    end
+    if(savePath.charAt(savePath.length-1) ~= '/')
+        savePath = savePath.concat('/');
+    end
+    if(logPath.charAt(logPath.length-1) ~= '/')
+        logPath = logPath.concat('/');
+    end
+    if(exist(strcat(char(logPath),'log.txt'),'file'))
+        delete(strcat(char(logPath),'log.txt'));
     end
     firstOne = 1;
     cf = 1; % Current file position
     newData = [];
-    if exist('log.txt','file')
-    	delete('log.txt');
-    end
     for f = 3:length(dirData)
         fileT = path.concat(dirData(f).name);
         if(fileT.substring(fileT.lastIndexOf('.')+1).equalsIgnoreCase('nc'))
@@ -30,15 +49,13 @@ function [] = dataProcessingERSST(dirName,var2Read)
                 % Catching data from original file
                 [timeDataSet,err] = readNC(fileT,var2Read);
                 if ~isnan(err)
-                    fid = fopen('log.txt', 'at+');
+                    fid = fopen(strcat(char(logPath),'log.txt'), 'at');
                     fprintf(fid, '[ERROR][%s] %s\n %s\n\n',char(datetime('now')),char(fileT),char(err));
                     fclose(fid);
                     continue;
                 end
                 %timeDataSet = nc_varget(char(fileT),var2Read);
                 if(firstOne == 1)
-                    %latDataSet = nc_varget(char(fileT),'lat');
-                    %lonDataSet = nc_varget(char(fileT),'lon');
                     [latDataSet,err] = readNC(fileT,'lat');
                     if ~isnan(err)
                         fid = fopen('log2.txt', 'at+');
@@ -56,67 +73,78 @@ function [] = dataProcessingERSST(dirName,var2Read)
                     newName = strcat('ERSST.v4.nc');
                     firstOne = 0;
                     
+                    % Catching data from original file
+                    ncoid = netcdf.open(char(fileT));
+                    GLOBALNC = netcdf.getConstant('NC_GLOBAL');
+                    
                     % New file configuration
-                    newFile = char(path.concat(newName));
-                    nc_create_empty(newFile,'netcdf4-classic');
+                    newFile = char(savePath.concat(newName));
+                    if exist(newFile,'file')
+                        delete(newFile);
+                    end
+                    ncid = netcdf.create(newFile,'NETCDF4');
 
                     % Adding file dimensions
-                    nc_add_dimension(newFile,'lat',length(latDataSet));
-                    nc_add_dimension(newFile,'lon',length(lonDataSet));
-                    nc_add_dimension(newFile,'time',0); % 0 means UNLIMITED dimension
+                    latdimID = netcdf.defDim(ncid,'lat',length(latDataSet));
+                    londimID = netcdf.defDim(ncid,'lon',length(lonDataSet));
+                    timedimID = netcdf.defDim(ncid,'time',netcdf.getConstant('NC_UNLIMITED'));
 
                     % Global params
-                    nc_attput(newFile,nc_global,'id',nc_attget(char(fileT),nc_global,'id'));
-                    nc_attput(newFile,nc_global,'naming_authority',nc_attget(char(fileT),nc_global,'naming_authority'));
-                    nc_attput(newFile,nc_global,'title',nc_attget(char(fileT),nc_global,'title'));
-                    nc_attput(newFile,nc_global,'institution',nc_attget(char(fileT),nc_global,'institution'));
-                    nc_attput(newFile,nc_global,'production_version',nc_attget(char(fileT),nc_global,'production_version'));
-                    nc_attput(newFile,nc_global,'cdm_data_type',nc_attget(char(fileT),nc_global,'cdm_data_type'));
-                    nc_attput(newFile,nc_global,'processing_level',nc_attget(char(fileT),nc_global,'processing_level'));
-                    nc_attput(newFile,nc_global,'source',nc_attget(char(fileT),nc_global,'source'));
-                    nc_attput(newFile,nc_global,'frequency','monthly');
-                    nc_attput(newFile,nc_global,'data_analysis_institution','CIGEFI - Universidad de Costa Rica');
-                    nc_attput(newFile,nc_global,'data_analysis_date',char(datetime('today')));
-                    nc_attput(newFile,nc_global,'data_analysis_contact','Roberto Villegas D: roberto.villegas@ucr.ac.cr');
+                    netcdf.copyAtt(ncoid,GLOBALNC,'id',ncid,GLOBALNC);
+                    netcdf.copyAtt(ncoid,GLOBALNC,'naming_authority',ncid,GLOBALNC);
+                    netcdf.copyAtt(ncoid,GLOBALNC,'title',ncid,GLOBALNC);
+                    netcdf.copyAtt(ncoid,GLOBALNC,'institution',ncid,GLOBALNC);
+                    netcdf.copyAtt(ncoid,GLOBALNC,'production_version',ncid,GLOBALNC);
+                    netcdf.copyAtt(ncoid,GLOBALNC,'cdm_data_type',ncid,GLOBALNC);
+                    netcdf.copyAtt(ncoid,GLOBALNC,'processing_level',ncid,GLOBALNC);
+                    netcdf.copyAtt(ncoid,GLOBALNC,'source',ncid,GLOBALNC);
+                    %netcdf.putAtt(ncid,GLOBALNC,'frequency','monthly');
+                    netcdf.putAtt(ncid,GLOBALNC,'data_analysis_institution','CIGEFI - Universidad de Costa Rica');
+                    netcdf.putAtt(ncid,GLOBALNC,'data_analysis_institution',char(datetime('today')));
+                    netcdf.putAtt(ncid,GLOBALNC,'data_analysis_contact','Roberto Villegas D: roberto.villegas@ucr.ac.cr');
 
                     % Adding file variables
-                    monthlyData.Name = var2Read;
-                    monthlyData.Datatype = 'single';
-                    monthlyData.Dimension = {'lat', 'lon','time'};
-                    nc_addvar(newFile,monthlyData);
+                    monthlyvarID = netcdf.defVar(ncid,var2Read,'float',[latdimID,londimID,timedimID]);
+                    [~] = netcdf.defVar(ncid,'time','float',timedimID);
+                    latvarID = netcdf.defVar(ncid,'lat','float',latdimID);
+                    lonvarID = netcdf.defVar(ncid,'lon','float',londimID);
 
-                    timeData.Name = 'time';
-                    timeData.Dimension = {'time'};
-                    nc_addvar(newFile,timeData);
-
-                    latData.Name = 'lat';
-                    latData.Dimension = {'lat'};
-                    nc_addvar(newFile,latData);
-
-                    lonData.Name = 'lon';
-                    lonData.Dimension = {'lon'};
-                    nc_addvar(newFile,lonData);
+                    netcdf.endDef(ncid);
 
                     % Writing the data into file
-                    nc_varput(newFile,'lat',latDataSet);
-                    nc_varput(newFile,'lon',lonDataSet);
+                    netcdf.putVar(ncid,latvarID,latDataSet);
+                    netcdf.putVar(ncid,lonvarID,lonDataSet);
                 end
                 newData = cat(3,newData,timeDataSet);
                 cf = cf +1;
-                [~,~,t] = size(newData);
-                %Writing the data into file
-                if t > 1
-                    nc_varput(newFile,var2Read,newData);
-                end
+%                 [~,~,t] = size(newData);
+%                 %Writing the data into file
+%                 if t > 1
+%                     nc_varput(newFile,var2Read,newData);
+%                 end
                 if(mod(cf,100)==0)
                     disp(strcat('Data saved:  ',char(fileT.substring(fileT.lastIndexOf('/')+1))));
                 end
             catch exception
+                netcdf.close(ncid);
+                netcdf.close(ncoid);
                 fid = fopen('log2.txt', 'at+');
                 fprintf(fid, '[ERROR][%s] %s\n %s\n\n',char(datetime('now')),char(fileT),char(exception.message));
                 fclose(fid);
             end
         end
+    end
+    try
+        % Writing the data into file
+        netcdf.putVar(ncid,monthlyvarID,[0 0 0],[length(latDataSet) length(lonDataSet) length(newData(1,1,:))],newData);
+        netcdf.close(ncid);
+        netcdf.close(ncoid);
+    catch exception
+        netcdf.close(ncid);
+        netcdf.close(ncoid);
+        fid = fopen('log2.txt', 'at+');
+        fprintf(fid, '[ERROR][%s] %s\n %s\n\n',char(datetime('now')),char(fileT),char(exception.message));
+        fclose(fid);
     end
 end
 function [data,error] = readNC(path,var2Read)
